28c895ea3eed86e0f938a04b3fd5d1c3edd6249d
lily <1452631814@qq.com>
2016-11-08 11:20:02 +0800

    add test

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Natukun on 2016-10-02.
+ */
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        smartSpeed: 1200,
+        autoplayHoverPause: true
+    });
+});
